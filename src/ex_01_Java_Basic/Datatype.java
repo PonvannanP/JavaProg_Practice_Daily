f8d9a98e8bbd179ac706bd0adbb119c78be3adcf
ponvannan.p <rajaponvan666@gmail.com>
2025-03-16 03:16:19 +0530

    10th March, - Pushing to Git

--- src/ex_01_Java_Basic/Datatype.java
+++ src/ex_01_Java_Basic/Datatype.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basic;
 
-public class _Java$Program {
+public class Datatype {
     public static void main(String[] args) {
         int table=18;
         System.out.printf("You variable is %d",table);
