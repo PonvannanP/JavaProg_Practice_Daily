7ac700d3543ef598ae8ab7a05a789fc3a276fb43
ponvannan.p <rajaponvan666@gmail.com>
2025-03-16 03:09:57 +0530

    10th March, - Pushing to Git

---
+++ src/ex_01_Java_Basic/_Java$Program.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class _Java$Program {
+    public static void main(String[] args) {
+        int table=18;
+        System.out.printf("You variable is %d",table);
+
+    }
+}
+
