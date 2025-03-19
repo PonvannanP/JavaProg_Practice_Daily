afafd2b7afbde13957c468f713c51d6e3f35f968
ponvannan.p <rajaponvan666@gmail.com>
2025-03-19 14:21:23 +0530

    10th March, - Pushing to Git

---
+++ src/ex_03_Java_Operators/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package ex_03_Java_Operators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        float c=1.5F;//for float it will be represented as either 'f' or 'F'
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a/c);//If you divide a float value it will give float digit
+    }
+}
