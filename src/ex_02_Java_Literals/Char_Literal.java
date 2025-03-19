c67bc31d9b89b6fb47b161126d459772a3125402
ponvannan.p <rajaponvan666@gmail.com>
2025-03-19 11:59:07 +0530

    10th March, - Pushing to Git

--- src/ex_02_Java_Literals/Char_Literal.java
+++ src/ex_02_Java_Literals/Char_Literal.java
@@ -6,9 +6,9 @@ public class Char_Literal {
         char tab_line='\t';
         char back_space='\b';
         char carraige_return='\r';
-        System.out.println("Pramod"+'\n'+"Dutta");//\n will print to next line
-        System.out.println("Pramod"+'\t'+"Dutta");// \t will provide space inbetween
-        System.out.println("Pramod"+'\b'+"Dutta");// \b will delete space inbetween
-        System.out.println("Pramod"+'\r'+"Dutta");// \r will delete the first word
+        System.out.println("Pon"+'\n'+"van");//\n will print to next line
+        System.out.println("Pon"+'\t'+"van");// \t will provide space inbetween
+        System.out.println("Pon"+'\b'+"van");// \b will delete space inbetween
+        System.out.println("Pon"+'\r'+"van");// \r will delete the first word
     }
 }
